0793938c1472074f155a19e01a996a90e8ddf030
Aline Galvão <alinegs93@gmail.com>
2022-10-26 23:09:58 -0300

    adicionando main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import router from "./router";
 import { RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-<RouterProvider />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
